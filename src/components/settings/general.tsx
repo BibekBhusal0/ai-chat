157e20f68603c49dc6786066d2da8501446cf979
BibekBhusal0 <bibekbhusal04@gmail.com>
2025-05-21 23:19:06 +0545

    feat: improve settings UI (not using tabs)

--- src/components/settings/general.tsx
+++ src/components/settings/general.tsx
@@ -1,6 +1,36 @@
+import { Select, SelectItem, Textarea } from "@heroui/react";
+import { useTheme } from "@heroui/use-theme";
+import { useChatStore } from "../../store/chatStore";
+
+const languages = [
+  'Auto',
+  'English',
+  'French',
+  'Spanish',
+  'German',
+  'Italian',
+  'Japanese',
+  'Chinese'
+];
+
 export default function General() {
+  const { theme, setTheme } = useTheme();
+  const { models } = useChatStore();
+
   return (
-    <div>This is general settings</div>
-  )
+    <div>
+      <Select selectedKeys={[theme]} onChange={e => setTheme(e.target.value)} label='Theme' labelPlacement='outside-left'>
+        <SelectItem key='light'>Light</SelectItem>
+        <SelectItem key='dark'>Dark</SelectItem>
+      </Select>
+      <Select label="Language" labelPlacement="outside-left" defaultSelectedKeys={["Auto"]} >
+        {languages.map((lang ) => (<SelectItem key={lang}>{lang}</SelectItem>))}
+      </Select>
+      <Select label="Default Model" labelPlacement="outside-left" defaultSelectedKeys={["gpt-4"]} >
+        {models.map(({id,name} ) => (<SelectItem key={id}>{name}</SelectItem>))}
+      </Select>
+      <Textarea label= 'System Prompt' placeholder = 'your system prompt here' labelPlacement = 'outside' />
+    </div>
+  );
 }
 
--- src/components/settings/index.tsx
+++ src/components/settings/index.tsx
@@ -1,59 +1,63 @@
-import { FC, ReactNode, } from "react";
-import { Modal, ModalBody, ModalContent, ModalHeader, Tab, Tabs } from "@heroui/react";
+import { FC, ReactNode, useState, } from "react";
+import { Modal, ModalBody, ModalContent, ModalHeader, ModalFooter, Button } from "@heroui/react";
 import { SettingsIcon } from "../icon/settings2";
 import General from "./general";
 import { FlaskIcon } from "../icon/flask";
 import Experimental from "./experimental";
-import { AnimatedDiv } from "../animatedDiv";
-import { Icon } from "@iconify/react";
+import { AnimatedIconButton } from "../animatedButton";
 
 type SettingsProps = { onOpenChange: () => void; isOpen: boolean };
 
 type setting = {
   icon: ReactNode;
-  title: ReactNode;
-  children: ReactNode;
+  title: string;
+  content: ReactNode;
 }
 
 const settings: setting[] = [
-  { icon: <SettingsIcon />, title: "General", children: <General /> },
-  { icon: <FlaskIcon />, title: "Experimental", children: <Experimental /> },
+  { icon: <SettingsIcon />, title: "General", content: <General /> },
+  { icon: <FlaskIcon />, title: "Experimental", content: <Experimental /> },
 ]
 
-
 export const Settings: FC<SettingsProps> = ({ onOpenChange, isOpen }) => {
-
-  const cls = 'flex items-center gap-2 size-full px-2 py-1'
+  const [selectedIndex, setSelectedIndex] = useState(0);
   const size = 15
   return (
-    <Modal size="2xl" isOpen={isOpen} onOpenChange={onOpenChange}>
+    <Modal backdrop='blur' size="2xl" isOpen={isOpen} onOpenChange={onOpenChange}>
       <ModalContent>
-        <ModalHeader >
-          Settings
-        </ModalHeader>
-        <ModalBody >
-          <Tabs
-            isVertical
-            variant = 'light'
-            classNames = {{
-              panel: 'w-full',
-              tab: 'p-0 m-0 relative',
-              tabContent: 'p-0 m-0 size-full',
+        {(onClose) => (<>
+          <ModalHeader >
+            Settings
+          </ModalHeader>
+          <ModalBody className="flex w-full flex-row gap-2">
+            <div className='flex flex-col gap-2'>
+              {settings.map(({ icon, title }, i) => {
+                return (
+                  <AnimatedIconButton
+                    icon={icon}
+                    variant={i === selectedIndex ? 'solid' : 'light'}
+                    className='align-center justify-start flex'
+                    size='sm'
+                    color={i === selectedIndex ? 'primary' : 'default'}
+                    onPress={() => setSelectedIndex(i)}
+                    key={i}
+                    iconSize={size}
+                    endContent={<div className='text-lg'>{title}</div>}
+                    isIconOnly={false}
+                  />
+                )
+              })}
+            </div>
 
-            }}
-          >
-            {settings.map(({ icon, title, children }) =>
-              <Tab
-                title={typeof icon === 'string' ? <div className={cls}>
-                  <Icon width = {size} height = {size} icon={icon} />{children}</div>
-                  : <AnimatedDiv iconSize = {size} className={cls} icon={icon}>{title}</AnimatedDiv>}
-              >
-                {children}
-              </Tab>
-            )}
-          </Tabs>
-
-        </ModalBody>
+            <div className='w-full p-1'>
+              {settings[selectedIndex].content}
+            </div>
+          </ModalBody>
+          <ModalFooter>
+            <Button variant='solid' color='primary'>Save</Button>
+            <Button variant='flat' color='danger' onPress={onClose}>Close</Button>
+          </ModalFooter>
+        </>)}
       </ModalContent>
     </Modal>
   );
