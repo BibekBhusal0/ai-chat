cb925f54143bfd4d3427f322b2f3328281b19322
BibekBhusal0 <bibekbhusal04@gmail.com>
2025-05-25 13:47:54 +0545

    feat: Animate ChatMessageEmpty text

--- src/components/chat/ChatMessageEmpty.tsx
+++ src/components/chat/ChatMessageEmpty.tsx
@@ -1,5 +1,6 @@
 import { cn } from "@heroui/react";
 import { Suggestions } from "./ChatSuggestedCommands.tsx";
+import { motion } from "motion/react";
 
 interface ChatMessagesProps {
   onSubmit: (message: string) => void;
@@ -9,15 +10,30 @@ export default function ChatMessageEmpty({ onSubmit }: ChatMessagesProps) {
   return (
     <div className="flex h-full flex-col justify-between p-4">
       <div />
-      <div
+      <motion.div
+        transition={{
+          duration: 0.5,
+          ease: "easeOut",
+        }}
+        initial={{
+          filter: "blur(10px)",
+          opacity: 0,
+          y: 20,
+        }}
+        animate={{
+          opacity: 1,
+          filter: "blur(0px)",
+          y: 0,
+        }}
         className={cn(
           "bg-size animate-bg-position p-2 text-center text-4xl font-semibold text-transparent",
           "bg-gradient-to-r from-warning-700 from-30% via-primary-700 via-50% to-danger-500 to-80% bg-[length:200%_auto] bg-clip-text"
         )}
       >
         How can I help you
-      </div>
+      </motion.div>
       <Suggestions onSubmit={onSubmit} />
     </div>
   );
 }
+
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,10 +9,25 @@ export default {
   ],
   theme: {
     extend: {
+      transitionTimingFunction: {
+        "minor-spring": "cubic-bezier(0.18,0.89,0.82,1.04)",
+      },
       keyframes: {
         "bg-position": {
           "0%": { backgroundPosition: "0% 50%" },
           "100%": { backgroundPosition: "100% 50%" },
+        },
+        "reveal-up": {
+          "0%": { opacity: "0", transform: "translateY(80%)" },
+          "100%": { opacity: "1", transform: "translateY(0)" },
+        },
+        "reveal-down": {
+          "0%": { opacity: "0", transform: "translateY(-80%)" },
+          "100%": { opacity: "1", transform: "translateY(0)" },
+        },
+        "content-blur": {
+          "0%": { filter: "blur(0.3rem)" },
+          "100%": { filter: "blur(0)" },
         },
       },
       animation: {
